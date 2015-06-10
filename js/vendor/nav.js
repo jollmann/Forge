2b98bd9a19b170872fe955e6be8c57b281814bc1
Jacob Ollmann <jacob.ollmann@zero-bars.com>
2015-06-10 17:05:00 -0500

    Forge Sidebar

---
+++ js/vendor/nav.js
@@ -0,0 +1,25 @@
+var menuLeft = document.getElementById( 'sidebar-right' ),
+    menuRight = document.getElementById( 'cbp-spmenu-s2' ),
+    menuTop = document.getElementById( 'cbp-spmenu-s3' ),
+    menuBottom = document.getElementById( 'cbp-spmenu-s4' ),
+    showLeft = document.getElementById( 'showLeft' ),
+    showRight = document.getElementById( 'showRight' ),
+    showTop = document.getElementById( 'showTop' ),
+    showBottom = document.getElementById( 'showBottom' ),
+    showLeftPush = document.getElementById( 'showLeftPush' ),
+    showRightPush = document.getElementById( 'showRightPush' ),
+    sidebar = document.getElementById( 'sidebar' );
+
+showLeftPush.onclick = function() {
+    classie.toggle( this, 'active' );
+    classie.toggle( sidebar, 'sidebar-right' );
+    classie.toggle( menuLeft, 'cbp-spmenu-open' );
+    disableOther( 'showLeftPush' );
+};
+
+
+function disableOther( button ) {
+    if( button !== 'showLeftPush' ) {
+        classie.toggle( showLeftPush, 'disabled' );
+    }
+}
